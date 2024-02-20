94444914b634b3aec263415a13813b691f51ab73
Aakash LS <aakashsarath@gmail.com>
2024-02-20 23:17:27 +0530

    Added smart contract for transaction record

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,35 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract MyContract {
-    constructor() {}
+contract KidneyDonorRegistry {
+    struct Donor {
+        uint id;
+        uint donorId;
+        uint hospitalId;
+        uint dateOfTransplant;
+        uint doctorId;
+        uint donorType;
+    }
+
+    mapping(uint => Donor) public donors;
+
+    function addDonor(
+        uint _id,
+        uint _donorId,
+        uint _hospitalId,
+        uint _dateOfTransplant,
+        uint _doctorId,
+        uint _donorType
+    ) public {
+        require(donors[_id].id == 0, "Record with the same ID already exists");
+
+        donors[_id] = Donor({
+            id: _id,
+            donorId: _donorId,
+            hospitalId: _hospitalId,
+            dateOfTransplant: _dateOfTransplant,
+            doctorId: _doctorId,
+            donorType: _donorType
+        });
+    }
 }
